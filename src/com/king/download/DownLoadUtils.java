9890133794e0c3518bba41c2746944c3b1f855f8
殇之弑梦 <1782635977@qq.com>
2019-08-02 13:23:18 +0800

    下载文件

---
+++ src/com/king/download/DownLoadUtils.java
@@ -0,0 +1,26 @@
+package com.king.download;
+
+import java.util.Arrays;
+import java.util.Base64;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+
+public class DownLoadUtils {
+
+    public static String getFileName(String agent, String filename) throws UnsupportedEncodingException {
+        if (agent.contains("MSIE")) {
+            // IE浏览器
+            filename = URLEncoder.encode(filename, "utf-8");
+            filename = filename.replace("+", " ");
+        } else if (agent.contains("Firefox")) {
+            // 火狐浏览器
+            Base64.Encoder encoder = Base64.getEncoder();
+            filename = "=?utf-8?B?" + Arrays.toString(encoder.encode(filename.getBytes("utf-8"))) + "?=";
+        } else {
+            // 其它浏览器
+            filename = URLEncoder.encode(filename, "utf-8");
+        }
+        return filename;
+    }
+}
